3719b0218128e1cd7f6a8a90b16bcda2644caceb
123shreyas <shreyasmagdum13@gmail.com>
2026-02-25 09:54:32 +0530

    Day14 and Day15

---
+++ Day14/LinqDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+var _context = new CrmContext();
+var customers = _context.Customers.ToList();
+
+public class CrmContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SampleDB;Integrated Security=True;TrustServerCertificate=True;");
+
+    }
+}
+
+public class Customer
+{
+     public int CustomerId { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+}
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public int CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+}
---
+++ Day15/UnitTesting/Program.cs
@@ -0,0 +1,56 @@
+﻿
+add2and2theoutputshouldbe4(2,2);
+add20and10theoutputshouldbe30(20,10);
+add22and11theoutputshouldbe33(22,11);
+
+
+void add2and2theoutputshouldbe4(int a, int b)
+{   
+    var Expected = 4;
+    var result = Add(a, b);
+    Console.WriteLine($"Expected Output: {Expected}, Actual Output: {result}");
+    if (result != Expected)
+    {
+        Console.WriteLine("Test failed");
+    }
+    else
+    {
+        Console.WriteLine("Test passed");
+    }
+}
+
+void add22and11theoutputshouldbe33(int a, int b)
+{   
+    var Expected = 33;
+    var result = Add(a, b);
+    Console.WriteLine($"Expected Output: {Expected}, Actual Output: {result}");
+    if (result != Expected)
+    {
+        Console.WriteLine("Test failed");
+    }
+    else
+    {
+        Console.WriteLine("Test passed");
+    }
+}
+
+void add20and10theoutputshouldbe30(int a, int b)
+{   
+    var Expected = 30;
+    var result = Add(a, b);
+    Console.WriteLine($"Expected Output: {Expected}, Actual Output: {result}");
+    if (result != Expected)
+    {
+        Console.WriteLine("Test failed");
+    }
+    else
+    {
+        Console.WriteLine("Test passed");
+    }
+}
+
+int Add(int x, int y)
+{
+    return x + y;
+    // return 0;
+}   
